04b1b709f5d5c224ffe78a67e1841a85a96dfcd0
Tal Jacob <taljacob2@gmail.com>
2023-04-04 00:16:15 +0300

    Add `getRandomNumberBetweenRange`
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 #include <ctime>
 #include <iostream>
 
-
+int getRandomNumberBetweenRange(int lowestValuePossible,
+                                int highestValuePossible) {
+    return lowestValuePossible +
+           rand() % (highestValuePossible - lowestValuePossible + 1);
+}
 
 int main() {
     srand((unsigned int) time(NULL));
 
     std::cout << "Hello, World!" << std::endl;
 
-    std::cout << rand() << std::endl;
+    std::cout << getRandomNumberBetweenRange(200, 300) << std::endl;
 
     return 0;
 }
