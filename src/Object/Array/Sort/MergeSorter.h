7a080f18674045019f6d40f52a3da770dcd6b5ee
Tal Jacob <taljacob2@gmail.com>
2023-04-04 02:09:37 +0300

    Init MergeSorter
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- src/Object/Array/Sort/MergeSorter.h
+++ src/Object/Array/Sort/MergeSorter.h
@@ -6,10 +6,29 @@
 template<typename E> class MergeSorter : public Sorter<E> {
 
   private:
-    void merge(E *array, size_t size) {}
+    void swap(E &first, E &second) {
+        E temp = first;
+        first  = second;
+        second = temp;
+    }
 
   private:
-    void mergeSort(E *array, size_t size) {}
+    void merge(E *firstArray, size_t firstArraySize, E *secondArray,
+               size_t secondArraySize) {
+
+    }
+
+  private:
+    // TODO: check odd size and even size.
+    void mergeSort(E *array, size_t size) {
+        E *           firstArray     = array;
+        unsigned long firstArraySize = size / 2;
+
+        E *           secondArray = array + size / 2;
+        unsigned long secondSize  = size / 2;
+
+        merge()
+    }
 
   public:
     void sort(E *array) override { mergeSort(array); }
