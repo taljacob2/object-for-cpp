c64cd7a3057fe4bdbb21715133ed2df5b6c8a9da
Tal Jacob <taljacob2@gmail.com>
2023-04-07 17:43:09 +0300

    Revert "Edit Size To 4 For Testing Purposes"
    This reverts commit bef4326087d7170b24a2662a3450fce7a5923c62.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main() {
 
     std::cout << "Hello, World!" << std::endl;
 
-    auto array = new BaseArray<int>(4);
+    auto array = new BaseArray<int>(getRandomNumberBetweenRange(200, 300));
 
     for (int i = 0; i < array->size(); ++i) {
         array->setElement((int &&) rand() % 20, i);
